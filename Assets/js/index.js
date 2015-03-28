d7d4506c63446f4bc6fab92707679cbf8ed55a71
frenatus <frenatus.tan@gmail.com>
2015-03-28 23:28:45 +0800

    pending api;

---
+++ Assets/js/index.js
@@ -0,0 +1,23 @@
+var canRegister = false;
+
+function onClickBtnLogin() {
+	var email = document.getElementById('loginEmail').value.trim();
+	var password = document.getElementById('loginPassword').value;
+}
+
+function onClickBtnRegister() {
+	if(canRegister) {
+		var email = document.getElementById('registerEmail').value.trim();
+		var password = document.getElementById('registerPassword').value;		
+	}
+}
+
+function onChangePassword() {
+	if(document.getElementById('registerPassword').value != document.getElementById('registerConfirm').value) {
+		canRegister = false;
+		document.getElementById('confirmPasswordErr').innerHTML = "*Passwords do not match!";
+	} else {
+		document.getElementById('confirmPasswordErr').innerHTML = "";
+		canRegister = true;
+	}
+}
